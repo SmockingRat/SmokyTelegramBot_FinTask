c022d06fff2e7cf10e48c4dc422d7b020f286cda
SmokingRat <halfbrikbarry@gmail.com>
2022-04-19 14:03:39 +0300

    Добавьте файлы проекта.

---
+++ SmokyTelegramBot_FinTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmokyTelegramBot_FinTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начали!");
+        }
+    }
+}
